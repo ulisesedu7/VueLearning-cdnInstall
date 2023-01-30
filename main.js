27c15bcfa49448a267c8524918d2b0daf8df8c30
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2023-01-29 20:17:32 -0600

    Create the vue version of the app

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+// Tenemos que crear la aplicacion
+Vue.createApp({
+  // Tenemos que definir la data
+  data() {
+    return {
+      goals: [],
+      enteredValue: '', // Conectarlo con HTML
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    } // El metodo para agregar goals
+  }
+}).mount('#app');
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = '';
-}
 
-buttonEl.addEventListener('click', addGoal);
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
